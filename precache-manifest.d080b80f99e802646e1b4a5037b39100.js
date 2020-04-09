f5ce5f5c4e4966a3052d47fab5872589f47de830
Alanch8 <aitor.lancharro@gmail.com>
2020-04-09 02:02:25 +0200

    Updates

---
+++ precache-manifest.d080b80f99e802646e1b4a5037b39100.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ae0de9ccdbbf33cbecd820b8a60dd9",
+    "url": "/alanch8.com/index.html"
+  },
+  {
+    "revision": "b8c4438941b51672adef",
+    "url": "/alanch8.com/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea553324a85c874290f3",
+    "url": "/alanch8.com/static/js/2.74ac4572.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alanch8.com/static/js/2.74ac4572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c4438941b51672adef",
+    "url": "/alanch8.com/static/js/main.93f9cb9e.chunk.js"
+  },
+  {
+    "revision": "8586ffbf87029f207fcf",
+    "url": "/alanch8.com/static/js/runtime-main.d2889d02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alanch8.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
